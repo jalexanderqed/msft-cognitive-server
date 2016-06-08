2fc51c96570028e093de8489583028c84eec9464
John T. Alexander <jalexander.qed@gmail.com>
2016-06-08 14:00:14 -0700

    Started on file read test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * Created by jalexander on 6/8/2016.
  */
 public class Main {
     public static void main(String[] args){
-
+        for(String line : Files.readAllLines(Paths.get("secret-test.txt")));
     }
 }
